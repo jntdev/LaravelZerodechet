6dc9abdd7d87016ae61b13506e320e6ea21171f1
Jonathan Marois <jnt.marois@gmail.com>
2021-11-24 14:33:46 +0100

    news du 24novembre

--- resources/views/actu.blade.php
+++ resources/views/actu.blade.php
@@ -3,89 +3,121 @@
     @include('/componant/header')
 
 <div class="actucontent">
+    <div class="actusection thirdbackground ">
+        <div class="article">
+            <div class="date flexend">
+                <p>24 novembre 2021</p>
+            </div>
+            <div class="pageactucontent ">
+                <div id="pict" class="pict">
+                    <img class="articleimg"src="{{ asset('images/mahe.jpg') }}" alt="illustration article">
+
+                </div>
+                <div class="articlecontent">
+                    <h2>Visite d'un circuit de bouteilles consignées</h2>
+                    <p id="p_responsiv" class="subtitleactucontent">Mardi 9 novembre, une dizaine de participants au défi a pu visiter le circuit de bouteilles consignées
+                        de la société Mahé à Pleubian.</p>
+                    <p id="p_responsiv" class="contenuactu">
+                        La visite leur a permis de découvrir le fonctionnement de la ligne d'embouteillage et de récupération
+                        de bouteilles de vin, qui permet de limiter déchets et pression sur les ressources. Une urgence, à
+                        l'heure où l'approvisionnement en verre mais aussi en eau deviennent problématiques.</br>
+                        Laurent Mahé, son dirigeant, leur a expliqué sa démarche et leur a fait découvrir les références
+                        consignées, dont quelques unes sont disponibles à la vente aux particiculiers.
+                        </br></br>Tous en sont ressortis enthousiastes !
+                    </p>
+
+                </div>
+            </div>
+            <div class="seemore flexend clickable">
+                <p>voir plus...</p>
+                <div class="arrow">
+                </div>
+            </div>
+        </div>
+    </div>
 <div class="actusection secondbackground ">
         <div class="article">
-           <div class="date flexend"> 
-                <p>29 octobre 2021</p>    
+           <div class="date flexend">
+                <p>29 octobre 2021</p>
            </div>
            <div class="pageactucontent ">
                <div id="pict" class="pict">
                <img class="articleimg"src="../images/ledefi.png" alt="illustration article">
-                   
+
                </div>
                <div class="articlecontent">
                    <h2>Lancement du défi sous le soleil !</h2>
-                   <p id="p_responsiv" class="subtitleactucontent">Ce sont finalement 59 foyers qui se lancent dans le défi zéro déchet du Goëlo !</p> 
-                    <p id="p_responsiv" class="contenuactu">
-                    Ils se sont réunis dimanche 17 octobre pour débuter cette aventure de 6 mois. Après la conférence
-de Laëtitia Crnkovic, autrice et fondatrice de Zéro déchet Trégor, ils ont partagé un apéritif et un
-pique-nique zéro déchet conviviaux. Puis, ils se sont regroupés par équipes en ateliers découvertes,
-pilotés par Julian Pondaven du réseau Cohérence, et ont pu échanger avec Emilie (Ecolégram') et
-Céline (Le Monde de Céline), commerçantes zéro déchet. </br></br>Leur prochain RDV se fera en équipes tout au long du mois de novembre.</br></br>
-Bravo à eux pour ce challenge et RDV dans 6 mois !
+                   <p id="p_responsiv" class="subtitleactucontent">Ce sont finalement 59 foyers qui se lancent dans le défi zéro déchet du Goëlo !</p>
+                   <p id="p_responsiv" class="contenuactu">
+                       Ils se sont réunis dimanche 17 octobre pour débuter cette aventure de 6 mois. Après la conférence
+                       de Laëtitia Crnkovic, autrice et fondatrice de Zéro déchet Trégor, ils ont partagé un apéritif et un
+                       pique-nique zéro déchet conviviaux. Puis, ils se sont regroupés par équipes en ateliers découvertes,
+                       pilotés par Julian Pondaven du réseau Cohérence, et ont pu échanger avec Emilie (Ecolégram') et
+                       Céline (Le Monde de Céline), commerçantes zéro déchet. </br></br>Leur prochain RDV se fera en équipes tout au long du mois de novembre.</br></br>
+                       Bravo à eux pour ce challenge et RDV dans 6 mois !
 
-Articles de presse <a target="_blank"href="https://actu.fr/bretagne/paimpol_22162/paimpol-59-foyers-participeront-au-defi-zero-dechet_45805649.html">ICI</a> et <a target="_blank" href="https://www.ouest-france.fr/bretagne/paimpol-22500/a-paimpol-55-personnes-veulent-reduire-leurs-dechets-de-moitie-en-six-mois-08b4307c-2f2e-11ec-a84f-a30e5f8a83f2">LA</a>
-<img src="../images/lancement.jpg" alt="">
+                       Articles de presse <a target="_blank"href="https://actu.fr/bretagne/paimpol_22162/paimpol-59-foyers-participeront-au-defi-zero-dechet_45805649.html">ICI</a> et <a target="_blank" href="https://www.ouest-france.fr/bretagne/paimpol-22500/a-paimpol-55-personnes-veulent-reduire-leurs-dechets-de-moitie-en-six-mois-08b4307c-2f2e-11ec-a84f-a30e5f8a83f2">LA</a>
+                       <img src="../images/lancement.jpg" alt="">
 
-                    </p>
-                    
+                   </p>
+
+               </div>
+           </div>
+            <div class="seemore flexend clickable">
+                <p>voir plus...</p>
+                <div class="arrow">
                 </div>
-           </div>
-           <div class="seemore flexend clickable">
-               <p>voir plus...</p>
-               <div class="arrow">
-               </div>
-            </div> 
+            </div>
         </div>
-    </div>
-<div class="actusection primebackground ">
+</div>
+    <div class="actusection primebackground ">
         <div class="article">
-           <div class="date flexend"> 
-                <p>16 octobre 2021</p>    
-           </div>
-           <div class="pageactucontent ">
-               <div id="pict" class="pict">
-               <img class="articleimg"src="../images/logo.png" alt="illustration article">
-                   
-               </div>
-               <div class="articlecontent">
-                   <h2>Les BTS 2 ESF de Kerraoul dessinent le logo d'Osez zéro déchet !</h2>
-                   <p id="p_responsiv" class="subtitleactucontent">Les élèves de BTS 2 ESF au lycée de Kerraoul se sont aussi lancé un défi... Réaliser le logo de
-l'association. </br> Depuis la rentrée, elles travaillent sur ce projet en cours de Design de communication. Nous avons
-proposé aux visiteurs de voter pour leur logo préféré, lors d'Un pas pour ma planète à Paimpol.</p> 
+            <div class="date flexend">
+                <p>16 octobre 2021</p>
+            </div>
+            <div class="pageactucontent ">
+                <div id="pict" class="pict">
+                    <img class="articleimg"src="../images/logo.png" alt="illustration article">
+
+                </div>
+                <div class="articlecontent">
+                    <h2>Les BTS 2 ESF de Kerraoul dessinent le logo d'Osez zéro déchet !</h2>
+                    <p id="p_responsiv" class="subtitleactucontent">Les élèves de BTS 2 ESF au lycée de Kerraoul se sont aussi lancé un défi... Réaliser le logo de
+                        l'association. </br> Depuis la rentrée, elles travaillent sur ce projet en cours de Design de communication. Nous avons
+                        proposé aux visiteurs de voter pour leur logo préféré, lors d'Un pas pour ma planète à Paimpol.</p>
                     <p id="p_responsiv" class="contenuactu">
-                    Le logo qui a obtenu le plus de votes est celui de Lucille. Il sera retravaillé et vous pourrez bientôt
-le découvrir en entête du site web et sur Facebook. </br></br>Un grand merci à Anaïs, Anna, Belinda, Bleuenn, Camille, Chloé, Clémence, Coraline, Elise,
-Flavie, Klervi, Léa, Lucille, Juliette, Justine, Maïwenn, Manon, Marie-Sue, Marion, Solène et leur
-professeure, Céline Rougé !
+                        Le logo qui a obtenu le plus de votes est celui de Lucille. Il sera retravaillé et vous pourrez bientôt
+                        le découvrir en entête du site web et sur Facebook. </br></br>Un grand merci à Anaïs, Anna, Belinda, Bleuenn, Camille, Chloé, Clémence, Coraline, Elise,
+                        Flavie, Klervi, Léa, Lucille, Juliette, Justine, Maïwenn, Manon, Marie-Sue, Marion, Solène et leur
+                        professeure, Céline Rougé !
                     </p>
                     <iframe src="https://padlet.com/embed/c4fpsr9uph3v3vxg"  style="width:100%;height:608px;display:block;padding:0;margin:0">
                     </iframe>
                 </div>
-           </div>
-           <div class="seemore flexend clickable">
-               <p>voir plus...</p>
-               <div class="arrow">
-               </div>
-            </div> 
+            </div>
+            <div class="seemore flexend clickable">
+                <p>voir plus...</p>
+                <div class="arrow">
+                </div>
+            </div>
         </div>
     </div>
     <div class="actusection secondbackground ">
         <div class="article">
-           <div class="date flexend"> 
-                <p>28 Septembre 2021</p>    
+            <div class="date flexend">
+                <p>28 Septembre 2021</p>
            </div>
            <div class="pageactucontent ">
                <div id="pict" class="pict">
                <img class="articleimg"src="../images/unpaspourmaplanete.jpeg" alt="affiche de l'évenement un pas pour ma planete">
-                   
+
                </div>
                <div class="articlecontent">
                    <h2>Osez zéro déchet ! présent à Un pas pour ma planète !</h2>
                    <p id="p_responsiv" class="subtitleactucontent">Nous participerons en octobre à la seconde édition d'Un pas pour ma planète !
                     Guingamp-Paimpol agglomération et l'association Cohérence organisent 21 jours d'animation, du 18
                     septembre au 8 octobre 2021, pour mettre en valeur les initiatives locales en faveur de la transition
-                    écologique.</p> 
+                    écologique.</p>
                     <p id="p_responsiv" class="contenuactu">
                     A Paimpol, cet événement se concrétisera notamment par une journée de rencontres et d'ateliers
                     organisée par le ChatÔ le 2 octobre. Seront également présents : J'accueille la nature, Bretagne
@@ -102,24 +134,24 @@ professeure, Céline Rougé !
                <p>voir plus...</p>
                <div class="arrow">
                </div>
-            </div> 
+            </div>
         </div>
     </div>
     <div class="actusection thirdbackground ">
         <div class="article">
-           <div class="date flexend"> 
-                <p>14 Septembre 2021</p>    
+           <div class="date flexend">
+                <p>14 Septembre 2021</p>
            </div>
            <div class="pageactucontent ">
                <div id="pict" class="pict">
                <img class="articleimg"src="../images/question_mark.png" alt="point d interrogation">
-                   
+
                </div>
                <div class="articlecontent">
                    <h2>Mais concrètement, le défi c'est quoi ?</h2>
                    <p id="p_responsiv" class="subtitleactucontent">Vous êtes intéressés par la démarche du défi mais vous ne savez pas trop en quoi ça consiste ? Vous
                     avez déjà mille choses à faire et vous avez peur que le défi ne vous prenne trop de temps ? On vous
-                    explique tout !</p> 
+                    explique tout !</p>
                     <p id="p_responsiv" class="contenuactu">
                     Participer au défi, c'est :</p>
                     <ul>
@@ -139,24 +171,24 @@ professeure, Céline Rougé !
                <p>voir plus...</p>
                <div class="arrow">
                </div>
-            </div> 
+            </div>
         </div>
     </div>
     <div class="actusection fourthbackground ">
         <div class="article">
-           <div class="date flexend"> 
-                <p>6 Septembre 2021</p>    
+           <div class="date flexend">
+                <p>6 Septembre 2021</p>
            </div>
            <div class="pageactucontent ">
                <div id="pict" class="pict">
                <img class="articleimg"src="../images/ledefi.png" alt="poing levé en guise de courage fédérateur">
-                   
+
                </div>
                <div class="articlecontent">
                    <h2>Du 6 septembre au 6 octobre : ouverture des inscriptions au défi !</h2>
                    <p id="p_responsiv" class="subtitleactucontent">Habitant(es) du Goëlo, un défi vous est lancé !</br>
                         Pro du zéro déchet ? Habitant sensibilisé et qui veut aller plus loin ? Débutant complet ? Peu
-                        importe ! Nous vous proposons de réduire vos déchets de 50 % en six mois...</p> 
+                        importe ! Nous vous proposons de réduire vos déchets de 50 % en six mois...</p>
                     <p id="p_responsiv" class="contenuactu">Lancez-vous dans l'aventure ! Par équipes, grâce à des visites, des ateliers et un partage des trucs et
                         astuces - le tout dans la bonne humeur - relevez ce challenge... Seul, en couple, avec les enfants...
                         résidant Bréhat, Kerfot, Lanleff, Lanloup, Paimpol, Pléhédel, Ploubazlanec, Plouézec, Plourivo ou
@@ -166,19 +198,19 @@ professeure, Céline Rougé !
                         Rendez-vous sur la page <a target="_blank" href="../docs/inscription.pdf">inscription</a>, du 6 septembre au 6 octobre 2021 !
                     </p>
                 </div>
-               
+
            </div>
            <div class="seemore flexend clickable">
                <p>voir plus...</p>
                <div class="arrow">
                </div>
-            </div> 
+            </div>
         </div>
     </div>
     <div class="actusection fifthbackground">
         <div class="article">
-           <div class="date flexend"> 
-                <p>2 septembre 2021</p>    
+           <div class="date flexend">
+                <p>2 septembre 2021</p>
            </div>
            <div class="pageactucontent">
                <div id="pict"class="pict">
@@ -209,8 +241,8 @@ professeure, Céline Rougé !
     </div>
     <div class="actusection primebackground">
         <div class="article">
-           <div class="date flexend"> 
-                <p>31 Août 2021</p>    
+           <div class="date flexend">
+                <p>31 Août 2021</p>
            </div>
 
             <div class="pageactucontent">
