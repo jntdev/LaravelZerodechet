1d0aab9b97a5552f8f70be9d0d7d55a1054c2789
Jonathan Marois <jnt.marois@gmail.com>
2021-12-07 11:22:21 +0100

    notif fr

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
+    'reset' => 'Votre mot de passe à été modifié!',
+    'sent' => 'Le mail de récupération de mot de passe a bien été envoyé!',
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that email address.",
